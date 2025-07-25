12a9a505e69bf3e9d74f19d4ea264e40828e7c42
Jay Paygude <jaypaygude2@gmail.com>
2025-07-25 11:40:33 +0530

    Create calculator.cpp

---
+++ calculator.cpp
@@ -0,0 +1,21 @@
+int main(){
+     int  sum, mult, sub;
+    float divi, num1, num2;
+
+    cout<<"Enter first number: ";
+    cin>>num1;
+    cout<<"Enter second number: ";
+    cin>>num2;
+
+    sum=num1+num2;
+    cout<<sum <<endl;
+
+    sub=num1-num2;
+    cout<<sub <<endl;
+
+    mult=num1*num2;
+    cout<<mult <<endl;
+
+    divi=num1/num2;
+    cout<<divi <<endl;
+}
